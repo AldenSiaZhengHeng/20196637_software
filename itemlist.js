0c2f98bfb18d2b24cee9b6b3c6f17fd4a403e9db
AldenSiaZhengHeng <aldensia85@gmail.com>
2023-03-16 16:00:55 +0000

    add new item list selection

--- itemlist.js
+++ itemlist.js
@@ -3,15 +3,18 @@ class itemlist{
         this.item_list = {
             "1": "Iphone 14 pro",
             "2": "Samsung Galaxy S22",
-            "3": "ROG phone"
+            "3": "ASUS ROG phone 6",
+            "iphone":"Iphone 14 pro",
+            "samsung":"Samsung Galaxy S22",
+            "rog": "ASUS ROG phone 6"
         }
 
     }
 
     getitem(selected){
         const targeted = this.item_list[selected]
-        console.log("get item here")
-        console.log(targeted)
+        // console.log("get item here")
+        // console.log(targeted)
         return targeted
     }
 }
