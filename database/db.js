ef43dac0413ff11b8958e6bf9821376f89ecab48
AldenSiaZhengHeng <aldensia85@gmail.com>
2023-04-18 15:12:15 +0100

    Reconstru

--- database/db.js
+++ database/db.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose' )
 
+
+// connect the mongodb database with specific url
 const connectDB = async () => {
     try{
         await mongoose.connect('mongodb+srv://aldensia0207:aldensia0207@chatbot-database.jzc8grd.mongodb.net/registered_admin', {
