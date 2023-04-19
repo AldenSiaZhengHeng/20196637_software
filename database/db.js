3712ecb16fd4f879b330372110ac9e418eabd8fa
AldenSiaZhengHeng <aldensia85@gmail.com>
2023-04-19 17:30:22 +0100

    add suggestion of database url

--- database/db.js
+++ database/db.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose' )
 // connect the mongodb database with specific url
 const connectDB = async () => {
     try{
+        // this will connect to the database of this project owner created
+        // if you are want to connect to own database, please modify this url
         await mongoose.connect('mongodb+srv://aldensia0207:aldensia0207@chatbot-database.jzc8grd.mongodb.net/registered_admin', {
             useUnifiedTopology: true,
             useNewUrlParser: true
